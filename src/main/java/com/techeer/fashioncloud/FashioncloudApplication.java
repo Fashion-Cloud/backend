b0ca2d85bb634ffd591f882117101c9813e5a3b7
jhkim <wellsy001012@gmail.com>
2023-03-17 16:32:33 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/techeer/fashioncloud/FashioncloudApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.fashioncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashioncloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashioncloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techeer/fashioncloud/FashioncloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.fashioncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashioncloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f4cd51adca7b0965f77a0c563aeeabb8d7f76741
jhkim <wellsy001012@gmail.com>
2023-03-21 06:33:24 +0900

    feat: BaseEntity, WeatherCode 추가

---
+++ src/main/java/com/techeer/fashioncloud/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.techeer.fashioncloud.global.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    //삭제 시점
+    private LocalDateTime deletedAt;
+}

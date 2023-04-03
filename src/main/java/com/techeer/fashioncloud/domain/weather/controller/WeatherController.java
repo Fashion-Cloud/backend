9a0acbcbfc62ea1c1abfec0cc260d6084647fd29
jhkim <wellsy001012@gmail.com>
2023-04-04 03:26:04 +0900

    refactor: 응답 형식 지정

--- src/main/java/com/techeer/fashioncloud/domain/weather/controller/WeatherController.java
+++ src/main/java/com/techeer/fashioncloud/domain/weather/controller/WeatherController.java
@@ -3,13 +3,15 @@ package com.techeer.fashioncloud.domain.weather.controller;
 import com.techeer.fashioncloud.domain.weather.dto.WeatherInfoResponse;
 import com.techeer.fashioncloud.domain.weather.position.Location;
 import com.techeer.fashioncloud.domain.weather.service.WeatherService;
+import com.techeer.fashioncloud.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.json.ParseException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.techeer.fashioncloud.global.dto.ApiResponse.ok;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,13 +22,13 @@ public class WeatherController {
     //현재 위도와 경도를 가지고 날씨를 받아오는 api
     //TODO : url에 현재 위도경도 정보 포함하면 사용자 위치 노출되므로 암호화하거나,, 다른 방법 찾기
     @GetMapping()
-    public ResponseEntity<WeatherInfoResponse> getWeatherHere(
+    public ApiResponse getWeatherHere(
             @RequestParam Double latitude,
             @RequestParam Double longitude
     ) throws ParseException, org.json.simple.parser.ParseException {
 
         WeatherInfoResponse weather = weatherService.getNowWeather(Location.getCoordinate(latitude, longitude));
-        return ResponseEntity.ok(weather);
+        return ok(weather);
 
     }
 

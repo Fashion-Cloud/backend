aff0670640667b4d089e1c4854c683b2962980d8
Ray Lee <wooo519@naver.com>
2023-04-10 01:40:52 +0900

    refactor : post service 영속성

--- src/main/java/com/techeer/fashioncloud/domain/post/service/PostService.java
+++ src/main/java/com/techeer/fashioncloud/domain/post/service/PostService.java
@@ -5,9 +5,12 @@ import com.techeer.fashioncloud.domain.post.dto.response.PostResponseDto;
 import com.techeer.fashioncloud.domain.post.entity.Post;
 import com.techeer.fashioncloud.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -15,37 +18,44 @@ import java.util.stream.Collectors;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
 
-    private final PostRepository postRepository;
+    private final PostRepository repository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
 
     public Post create(PostCreateServiceDto dto) {
-
-        // Save Request
-        Post entity = postRepository.save(Post.builder()
+        Post post = Post.builder()
                 .name(dto.getName())
                 .image(dto.getImage())
-                .skyStatus((dto.getSkyStatus()))
-                .temperature((dto.getTemperature()))
-                .humidity((dto.getHumidity()))
-                .rainfallType((dto.getRainfallType()))
-                .windSpeed((dto.getWindSpeed()))
-                .review((dto.getReview()))
-                .build());
-
-        return entity;
-    }
-
-    public Post findRequestById(UUID id) { // PostID로 검색
-        return postRepository.findById(id).orElseThrow(()->new IllegalArgumentException(("해당 게시글이 없습니다. id="+id))); // DTO를 거치고 나온 데이터
+                .skyStatus(dto.getSkyStatus())
+                .temperature(dto.getTemperature())
+                .humidity(dto.getHumidity())
+                .rainfallType(dto.getRainfallType())
+                .windSpeed(dto.getWindSpeed())
+                .review(dto.getReview())
+                .build();
+        Post savedPost = repository.save(post);
+        entityManager.flush();
+        return savedPost;
     }
 
     public List<PostResponseDto> findAllPosts() {
-        List<Post> posts = postRepository.findAll(); // 리스트 형식으로 전체 불러오기
-        return posts.stream().map(PostResponseDto::fromEntity).collect(Collectors.toList()); // map함수를 통해 하나하나 담기
+        return repository.findAll().stream()
+                .map(PostResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public Post findRequestById(UUID id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Post ID"));
     }
 
     public void deleteRequestById(UUID id) {
-        postRepository.deleteById(id);
+        repository.deleteById(id);
     }
+
 }

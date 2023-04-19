97f70509c39b83675391f744315730d2085ad33b
Ray Lee <wooo519@naver.com>
2023-04-20 01:26:12 +0900

    feat : businessException

---
+++ src/main/java/com/techeer/fashioncloud/domain/post/exception/PostNotFoundException.java
@@ -0,0 +1,8 @@
+package com.techeer.fashioncloud.domain.post.exception;
+
+import com.techeer.fashioncloud.global.error.ErrorCode;
+
+public class PostNotFoundException extends Valuex{
+    public PostNotFoundException(){
+    }
+}

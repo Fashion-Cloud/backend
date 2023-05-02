5c7d36fe39cd4b7f55e5957e3bdba889ec8383b2
수연 <syeon0131@naver.com>
2023-05-03 02:35:34 +0900

    feat:apiResponse

--- src/main/java/com/techeer/fashioncloud/global/response/ResponseCode.java
+++ src/main/java/com/techeer/fashioncloud/global/response/ResponseCode.java
@@ -10,8 +10,11 @@ public enum ResponseCode {
     POST_GET_SUCCESS(200, "게시글 조회 성공"),
 
     ADDRESS_GET_SUCCESS(200, "주소 조회 성공"),
-
-
+    CREATE_POST_SUCCESS(200, "게시글 생성 성공"),
+    POST_DELETE_SUCCESS(200, "게시글 삭제 성공"),
+    POST_PUT_SUCCESS(200, "게시글 업데이트 성공"),
+    IMAGE_POST_SUCCESS(200, "이미지 업로드 성공"),
+    IMAGE_GET_SUCCESS(200,"이미지 삭제 성공"),
     WEATHER_GET_SUCCESS(200, "날씨 조회 성공");
 
     private final Integer status;

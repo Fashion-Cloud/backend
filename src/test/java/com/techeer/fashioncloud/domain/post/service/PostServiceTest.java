fee5a664f2c7d5e7acef1ce12cfea8ade6161869
Ray Lee <wooo519@naver.com>
2023-05-04 00:01:11 +0900

    feat : PostCreatetest

--- src/test/java/com/techeer/fashioncloud/domain/post/service/PostServiceTest.java
+++ src/test/java/com/techeer/fashioncloud/domain/post/service/PostServiceTest.java
@@ -4,7 +4,7 @@ import com.techeer.fashioncloud.domain.post.dto.request.PostCreateServiceDto;
 import com.techeer.fashioncloud.domain.post.entity.Post;
 import com.techeer.fashioncloud.domain.post.entity.Review;
 import com.techeer.fashioncloud.domain.post.repository.PostRepository;
-import jakarta.transaction.Transactional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -18,20 +18,21 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.UUID;
 
-import static com.techeer.fashioncloud.domain.post.entity.Review.추웠다;
+import static com.techeer.fashioncloud.domain.post.entity.Review.더웠다;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles(profiles = {"test"})
 // Flush DB after each test
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class ReviewServiceTest {
+public class PostServiceTest {
 
     // 시간 관계상 Repository Test Code 작성하지 않아 Mocking도 하지 않음.
     @Autowired
-    private PostService reviewService;
+    private PostService postService;
     @Autowired
-    private PostRepository reviewRepository;
+    private PostRepository postRepository;
 
     private UUID givenUser;
     private String givenName;
@@ -43,27 +44,25 @@ public class ReviewServiceTest {
     private Review givenReview;
 
     @BeforeEach
-    @Transactional
     void setUp() {
         givenUser = UUID.fromString("f9ad3dee-0782-40ad-a513-0f9e8a26ed84");
         givenName = "aa";
         givenTemperature = 3.0;
         givenImage = "aaa";
         givenSkyStatus = 0;
-        givenReview = 추웠다;
+        givenReview = 더웠다;
         givenRainFallType = 3;
         givenWindSpeed = 7.0;
 
-        reviewRepository.flush();
+        postRepository.flush();
     }
 
     @Nested
     @DisplayName("REVIEW 생성 테스트")
-    class CreateReviewTest {
+    class PostCreateTest {
 
         @Test
         @DisplayName("성공")
-        @Transactional
         void createTest() {
             // given
             PostCreateServiceDto givenDto = PostCreateServiceDto.builder()
@@ -76,17 +75,16 @@ public class ReviewServiceTest {
                     .build();
 
             // when
-            Post actualPost = PostService.create(givenDto);
+            Post actualPost = postService.create(givenDto);
 
             // then
-            assertThat(actualPost.getUserId()).isEqualTo("Create content");
-            assertThat(actualPost.getName()).isEqualTo("11111");
-            assertThat(actualPost.getImage()).isEqualTo("11111");
-            assertTrue(actualPost.getSkyStatus() == 0);
-            assertTrue(actualPost.getRainfallType() == 0);
-            assertThat(actualPost.getReview()).isEqualTo("더웠다");
-            assertTrue(actualPost.getWindChill() == 0);
+            assertThat(actualPost.getUserId()).isEqualTo(givenUser);
+            assertThat(actualPost.getName()).isEqualTo(givenName);
+            assertThat(actualPost.getImage()).isEqualTo(givenImage);
+            assertThat(actualPost.getSkyStatus()).isEqualTo(givenSkyStatus);
+            assertThat(actualPost.getRainfallType()).isEqualTo(givenRainFallType);
+            assertThat(actualPost.getReview()).isEqualTo(givenReview);
+            assertThat(actualPost.getWindChill()).isEqualTo(0.0);
         }
     }
-
 }

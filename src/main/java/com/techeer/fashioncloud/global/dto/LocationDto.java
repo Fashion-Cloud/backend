c35fe0b2f57436f50f3f07305591172aab85f7e8
jhkim <wellsy001012@gmail.com>
2023-05-03 17:14:48 +0900

    refactor: 응답 형태 변경 및 응답 코드 변경

---
+++ src/main/java/com/techeer/fashioncloud/global/dto/LocationDto.java
@@ -0,0 +1,15 @@
+package com.techeer.fashioncloud.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationDto {
+    private Double latitude;
+    private Double longitude;
+}
--- src/main/java/com/techeer/fashioncloud/global/response/ResponseCode.java
+++ src/main/java/com/techeer/fashioncloud/global/response/ResponseCode.java
@@ -7,14 +7,16 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseCode {
 
+    //POST
     POST_GET_SUCCESS(200, "게시글 조회 성공"),
+    POST_CREATE_SUCCESS(201, "게시글 생성 성공"),
+    POST_DELETE_SUCCESS(204, "게시글 삭제 성공"),
+    POST_UPDATE_SUCCESS(200, "게시글 업데이트 성공"),
 
     ADDRESS_GET_SUCCESS(200, "주소 조회 성공"),
-    POST_CREATE_SUCCESS(200, "게시글 생성 성공"),
-    POST_DELETE_SUCCESS(200, "게시글 삭제 성공"),
-    POST_UPDATE_SUCCESS(200, "게시글 업데이트 성공"),
-    IMAGE_UPLOAD_SUCCESS(200, "이미지 업로드 성공"),
-    IMAGE_DELETE_SUCCESS(200,"이미지 삭제 성공"),
+
+    IMAGE_UPLOAD_SUCCESS(201, "이미지 업로드 성공"),
+    IMAGE_DELETE_SUCCESS(204,"이미지 삭제 성공"),
     WEATHER_GET_SUCCESS(200, "날씨 조회 성공");
 
     private final Integer status;

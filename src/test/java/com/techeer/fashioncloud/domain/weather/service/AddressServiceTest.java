295b040c04bb4fce3c5b8e41a607f114b1ec6a10
jhkim <wellsy001012@gmail.com>
2023-05-04 22:06:20 +0900

    feat: 테스트코드

---
+++ src/test/java/com/techeer/fashioncloud/domain/weather/service/AddressServiceTest.java
@@ -0,0 +1,7 @@
+package com.techeer.fashioncloud.domain.weather.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressServiceTest {
+
+}
---
+++ src/test/java/com/techeer/fashioncloud/domain/weather/service/WeatherServiceTest.java
@@ -0,0 +1,16 @@
+package com.techeer.fashioncloud.domain.weather.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class WeatherServiceTest {
+
+    @Test
+    @DisplayName("유효하지 않은 위도 및 경도 입력시 Exception발생")
+    public void invalidLocation() {
+
+    }
+
+
+
+}

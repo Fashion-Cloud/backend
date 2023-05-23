df933a220c379763f30953f51e1e99a625697b06
Ray Lee <wooo519@naver.com>
2023-05-23 22:42:32 +0900

    feat : LookBook table

---
+++ src/main/java/com/techeer/fashioncloud/domain/post/entity/LookBook.java
@@ -0,0 +1,32 @@
+package com.techeer.fashioncloud.domain.post.entity;
+
+import com.techeer.fashioncloud.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Setter
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+public class LookBook extends BaseEntity {
+
+    @Id
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "uuid2") // postgresql strategy 표기가 mysql과 차이가 있음.
+    @Column(length = 36, nullable = false, updatable = false)
+    private UUID id = UUID.randomUUID();
+
+    @NotNull
+    private String title;
+
+    public void update(String title) {
+        this.title = title;
+    }
+}

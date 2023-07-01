5176563e060081b9a136215546fba3deae4462e4
Ray Lee <wooo519@naver.com>
2023-07-02 01:11:00 +0900

    fix : controller 코드 수정

--- src/main/java/com/techeer/fashioncloud/domain/post/controller/LookBookController.java
+++ src/main/java/com/techeer/fashioncloud/domain/post/controller/LookBookController.java
@@ -7,7 +7,6 @@ import com.techeer.fashioncloud.domain.post.dto.response.LookBookPostResponseDto
 import com.techeer.fashioncloud.domain.post.dto.response.LookBookResponseDto;
 import com.techeer.fashioncloud.domain.post.entity.LookBook;
 import com.techeer.fashioncloud.domain.post.entity.LookBookPost;
-import com.techeer.fashioncloud.domain.post.entity.Post;
 import com.techeer.fashioncloud.domain.post.service.LookBookService;
 import com.techeer.fashioncloud.domain.post.service.PostService;
 import com.techeer.fashioncloud.global.response.ResponseCode;
@@ -30,34 +29,24 @@ public class LookBookController {
     public ResponseEntity<ResultResponse> lookBookCreate(
             @RequestBody LookBookCreateRequestDto dto
     ){
-        LookBook entity = bookService.bookCreate(dto);
+        LookBook entity = bookService.lookBookCreate(dto);
         LookBookResponseDto response = bookMapper.toBookResponseDto(entity);
 
-
         return ResponseEntity.ok(ResultResponse.of(ResponseCode.LOOK_BOOK_CREATE_SUCCESS, response));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ResultResponse> getOneLookBook(@PathVariable UUID id) {
-        return ResponseEntity.ok(ResultResponse.of(ResponseCode.LOOK_BOOK_GET_SUCCESS, bookService.findBookById(id)));
     }
 
     @PostMapping("posts")
     public ResponseEntity<ResultResponse> lookBookPostCreate(
             @RequestBody LookBookPostCreateRequestDto dto
     ){
-        LookBook lookBook = bookService.findBookById(dto.getLookBookId());
-        Post post = postService.findPostById(dto.getPostId());
-
-        LookBookPost entity = bookService.lookBookCreate(lookBook, post);
+        LookBookPost entity = bookService.lookBookPostCreate(dto);
         LookBookPostResponseDto response = bookMapper.toBookPostResponseDto(entity);
-
 
         return ResponseEntity.ok(ResultResponse.of(ResponseCode.LOOK_BOOK_POST_CREATE_SUCCESS, response));
     }
 
     @GetMapping("/posts/{id}")
-    public ResponseEntity<ResultResponse> getOneLookBookPost(@PathVariable UUID id) {
+    public ResponseEntity<ResultResponse> getOneLookBook(@PathVariable UUID id) {
         return ResponseEntity.ok(ResultResponse.of(ResponseCode.LOOK_BOOK_POST_GET_SUCCESS, bookService.findLookBookById(id)));
     }
 
--- src/main/java/com/techeer/fashioncloud/domain/post/service/LookBookService.java
+++ src/main/java/com/techeer/fashioncloud/domain/post/service/LookBookService.java
@@ -2,12 +2,12 @@ package com.techeer.fashioncloud.domain.post.service;
 
 import com.techeer.fashioncloud.domain.post.dto.mapper.LookBookMapper;
 import com.techeer.fashioncloud.domain.post.dto.request.LookBookCreateRequestDto;
+import com.techeer.fashioncloud.domain.post.dto.request.LookBookPostCreateRequestDto;
 import com.techeer.fashioncloud.domain.post.dto.response.LookBookPostDataResponseDto;
 import com.techeer.fashioncloud.domain.post.entity.LookBook;
 import com.techeer.fashioncloud.domain.post.entity.LookBookPost;
 import com.techeer.fashioncloud.domain.post.entity.Post;
 import com.techeer.fashioncloud.domain.post.exception.BookNotFoundException;
-import com.techeer.fashioncloud.domain.post.exception.LookBookPostNotFoundException;
 import com.techeer.fashioncloud.domain.post.repository.BookRepository;
 import com.techeer.fashioncloud.domain.post.repository.LookBookRepository;
 import lombok.RequiredArgsConstructor;
@@ -26,26 +26,30 @@ public class LookBookService {
     private final BookRepository bookRepository;
     private final LookBookRepository lookBookRepository;
     private final LookBookMapper lookBookMapper;
+    private final LookBookService bookService;
+    private final PostService postService;
 
-    public LookBook bookCreate(LookBookCreateRequestDto dto) {
+    public LookBook lookBookCreate(LookBookCreateRequestDto dto) {
         LookBook entity = bookRepository.save(LookBook.builder()
                 .title(dto.getTitle()).build());
         return entity;
     }
 
-    public LookBookPost lookBookCreate(LookBook lookBook, Post post) {
+    public LookBook findBookById(UUID id){
+        return bookRepository.findById(id).orElseThrow(()-> new BookNotFoundException());
+    }
+
+    public LookBookPost lookBookPostCreate(LookBookPostCreateRequestDto dto) {
+        LookBook lookBook = bookService.findBookById(dto.getLookBookId());
+        Post post = postService.findPostById(dto.getPostId());
         LookBookPost entity = lookBookRepository.save(LookBookPost.builder()
                 .lookBook(lookBook)
                 .post(post).build());
         return entity;
     }
 
-    public LookBook findBookById(UUID id) {
-        return bookRepository.findById(id).orElseThrow(()-> new BookNotFoundException());
-    }
-
     public List<LookBookPostDataResponseDto> findLookBookById(UUID id) {
-        LookBook lookBook = bookRepository.findById(id).orElseThrow(LookBookPostNotFoundException::new);
+        LookBook lookBook = bookService.findBookById(id);
         List<LookBookPost> lookbooklist = lookBookRepository.findByLookBook(lookBook);
         List<Post> postList = lookbooklist.stream().map(LookBookPost::getPost).collect(Collectors.toList());
 

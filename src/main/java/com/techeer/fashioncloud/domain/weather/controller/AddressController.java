4a2f5f1f65e87e5109ef83321ea26978717d5c33
jhkim <wellsy001012@gmail.com>
2023-08-14 21:59:57 +0900

    feat: CORS에러 해결

--- src/main/java/com/techeer/fashioncloud/domain/weather/controller/AddressController.java
+++ src/main/java/com/techeer/fashioncloud/domain/weather/controller/AddressController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/address")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AddressController {
 
     private final AddressService addressService;
---
+++ src/main/java/com/techeer/fashioncloud/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.techeer.fashioncloud.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    //CORS설정
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+                .allowedHeaders("headers")
+                .maxAge(3000);
+    }
+}

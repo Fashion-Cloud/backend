76e896364b66effe98ea1e02b7157cda8b1d2deb
jhkim <wellsy001012@gmail.com>
2023-08-29 01:33:57 +0900

    fix: 파일 충돌 해결

--- src/main/java/com/techeer/fashioncloud/domain/geo/controller/AddressController.java
+++ src/main/java/com/techeer/fashioncloud/domain/geo/controller/AddressController.java
@@ -4,6 +4,8 @@ import com.techeer.fashioncloud.domain.geo.dto.AddressResponse;
 import com.techeer.fashioncloud.domain.geo.service.AddressService;
 import com.techeer.fashioncloud.global.response.ResponseCode;
 import com.techeer.fashioncloud.global.response.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,10 +21,11 @@ public class AddressController {
     private final AddressService addressService;
 
     @GetMapping()
+    @Operation(summary = "위치 반환", description ="위경도를 이용해 위치를 반환한다.")
     public ResponseEntity<ResultResponse> getAddressHere(
-            @RequestParam Double latitude,
-            @RequestParam Double longitude
-            ) {
+            @Parameter(name="latitude") @RequestParam Double latitude,
+            @Parameter(name="longitude") @RequestParam Double longitude
+    ) {
         AddressResponse address = addressService.getAddress(latitude, longitude);
         return ResponseEntity.ok(ResultResponse.of(ResponseCode.ADDRESS_GET_SUCCESS, address));
     }

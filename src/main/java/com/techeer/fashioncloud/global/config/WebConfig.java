02304c02012caa3e42f61a97d4d42aa6e6ab32b3
jhkim <wellsy001012@gmail.com>
2023-09-24 18:39:02 +0900

    feat: authentication filter, token provider

--- src/main/java/com/techeer/fashioncloud/global/config/WebConfig.java
+++ src/main/java/com/techeer/fashioncloud/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     //CORS설정
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
                 .allowedHeaders("headers")

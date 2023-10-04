5820368cf8f50cdd8ddfd40b11c9834d0b992387
jhkim <wellsy001012@gmail.com>
2023-10-04 20:18:06 +0900

    fix: audit설정 추가

--- src/main/java/com/techeer/fashioncloud/FashioncloudApplication.java
+++ src/main/java/com/techeer/fashioncloud/FashioncloudApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 @EnableAspectJAutoProxy
+@EnableJpaAuditing
 public class FashioncloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FashioncloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FashioncloudApplication.class, args);
+    }
 
 }
--- src/main/java/com/techeer/fashioncloud/global/entity/BaseEntity.java
+++ src/main/java/com/techeer/fashioncloud/global/entity/BaseEntity.java
@@ -2,22 +2,23 @@ package com.techeer.fashioncloud.global.entity;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @UpdateTimestamp
     @LastModifiedDate
     private LocalDateTime updatedAt;
 

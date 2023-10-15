54bc16b2e5d9752c50a6c868f1d11cec212b8855
jhkim <wellsy001012@gmail.com>
2023-10-15 18:45:26 +0900

    docs: swagger에 jwt authentication 추가

--- src/main/java/com/techeer/fashioncloud/global/config/SwaggerConfig.java
+++ src/main/java/com/techeer/fashioncloud/global/config/SwaggerConfig.java
@@ -1,6 +1,10 @@
 package com.techeer.fashioncloud.global.config;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +12,25 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI reviewOpenAPI() {
-        return new OpenAPI().info(new Info()
+    public OpenAPI openAPI() {
+        Info info = new Info()
                 .title("FashionCloud API")
-                .description("API for FashionCloud Service"));
-    }
+                .description("API for FashionCloud Service");
 
+        String jwtSchemeName = "Bearer JWT Authentication";
+
+        // 요청 헤더에 인증 정보 포함
+        SecurityRequirement securityItem = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer").bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityItem)
+                .components(components);
+    }
 }
